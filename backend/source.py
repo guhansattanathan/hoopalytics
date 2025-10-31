978fb4375a1bda336743d6cbfa5f9e657d6c0873
Xiaoyu-Ch <xiaoyu.chen1@ufl.edu>
2025-10-31 10:04:54 -0400

    Update source.py
    changed file name. split to two files.
    solved critical loop index bug in merge

--- backend/source.py
+++ backend/source.py
@@ -2,6 +2,8 @@
 def calculateOne(coefficients: list[float], player_data: list[int]) -> float:
     # only for one player
     temp = min(len(coefficients), len(player_data))
+    #min says will only care for the length of the shorter one
+    #so the others are by default
     score = 0.0
     for i in range(temp):
         score = score + coefficients[i] * player_data[i]
@@ -33,17 +35,19 @@ def mergeSort(arr: list[float]) -> list[float]:
     sortedRight = mergeSort(right)  # divide and conquer
     return merge(sortedLeft, sortedRight)  # this is the key. call helper
 
-
+#merge . ork with the other function
+# get 2 compare one by one and put in.
+#base case of 1 is implied to be sorted
 def merge(left: list[float], right: list[float]) -> list[float]:
     res = []
     i, j = 0, 0
     while i < len(left) and j < len(right):
-        if left[i] > right[i]:
-            res.append(right[j])
-            j += 1
-        else:
+        if left[i] >= right[j]:
             res.append(left[i])
             i += 1
+        else:
+            res.append(right[j])
+            j += 1
     for a in left[i:]:
         res.append(a)
     for b in right[j:]:
