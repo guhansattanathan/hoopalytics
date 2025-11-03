5bc2906c191e0bc2e26efcd4b1fd6eb66c40812c
kylecannon12 <166737599+kylecannon12@users.noreply.github.com>
2025-11-03 22:48:48 +0000

    get player data function

--- backend/database.py
+++ backend/database.py
@@ -8,3 +8,22 @@ cursor = db.cursor()
 
 cursor.execute("SELECT * FROM game WHERE team_name_home = 'Toronto Huskies';")
 print(cursor.fetchall())
+
+def get_player_data(stat):
+
+    query = f"""
+        SELECT player_name, {stat}
+        FROM PlayerStats
+        WHERE {stat} IS NOT NULL
+        ORDER BY {stat} DESC
+    """
+
+    try:
+        with sqlite3.connect(db_path) as conn:
+            cursor.execute(query)
+            cursor.execute(query)
+            rows = cursor.fetchall()
+            return rows
+    except Exception:
+        print(f"Error: {Exception}")
+        return []
