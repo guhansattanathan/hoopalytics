f2a7e17df6dc44fbabec6eb5ec743fef20d68e23
kylecannon12 <166737599+kylecannon12@users.noreply.github.com>
2025-11-04 03:07:40 +0000

    different dataset

--- backend/database.py
+++ backend/database.py
@@ -1,29 +1,45 @@
 import sqlite3
 import os
+import pandas as pd
 
-db_path = os.path.join(os.path.dirname(__file__), "../data/nba.sqlite")
+csv_path = os.path.join(os.path.dirname(__file__), "../data/archive/PlayerStatistics.csv")
 
-db = sqlite3.connect(db_path)
-cursor = db.cursor()
-
-cursor.execute("SELECT * FROM game WHERE team_name_home = 'Toronto Huskies';")
-print(cursor.fetchall())
-
-def get_player_data(stat):
-
-    query = f"""
-        SELECT player_name, {stat}
-        FROM PlayerStats
-        WHERE {stat} IS NOT NULL
-        ORDER BY {stat} DESC
-    """
-
+def get_averages(stat='points', season_start='2025-10-01'):
     try:
-        with sqlite3.connect(db_path) as conn:
-            cursor.execute(query)
-            cursor.execute(query)
-            rows = cursor.fetchall()
-            return rows
-    except Exception:
-        print(f"Error: {Exception}")
-        return []
+        df = pd.read_csv(csv_path, usecols=['firstName', 'lastName', 'gameDate', 'points', 'reboundsTotal','assists', 
+                                            'blocks', 'steals', 'fieldGoalsPercentage', 'threePointersPercentage', 
+                                            'freeThrowsPercentage', 'plusMinusPoints'])
+        df['gameDate'] = pd.to_datetime(df['gameDate'], errors='coerce')
+        df = df[df['gameDate'] >= pd.Timestamp(season_start)]
+        df['Player'] = df['firstName'] + ' ' + df['lastName']
+
+        averages = (
+            df.groupby('Player', as_index=False)
+            .agg({'points': 'mean', 'assists': 'mean', 'blocks': 'mean'})
+            .round(1)
+        )
+
+        averages.rename(columns={'points': 'PPG', 'assists': 'APG', 'blocks': 'BPG'}, inplace=True)
+
+        sort_col = {'points': 'PPG', 'assists': 'APG', 'blocks': 'BPG'}[stat]
+        averages = averages.sort_values(sort_col, ascending=False)
+
+        return averages
+
+    except Exception as e:
+        print(f"Error loading data: {e}")
+        return pd.DataFrame()
+
+
+
+if __name__ == "__main__":
+    with sqlite3.connect(db_path) as conn:
+        cur = conn.cursor()
+        cur.execute("SELECT name FROM sqlite_master WHERE type='table';")
+        print("Tables in this database:")
+        for table in cur.fetchall():
+            print(" -", table[0])
+
+    print("\nTop players by PPG:")
+    df = get_player_data("PTS", season_start="2025-10-15")
+    print(df.head(10))
